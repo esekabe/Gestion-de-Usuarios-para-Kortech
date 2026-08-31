eca067559120377e3784f1d5ba9f865e667ef2e6
esekabe <djroastermusic@gmail.com>
2026-08-31 17:40:51 -0400

    Implementacion de Tabla al Iniciar Sesion

--- react-front/src/App.jsx
+++ react-front/src/App.jsx
@@ -1,20 +1,22 @@
 import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
 import { Login } from './pages/Login';
+import { UserList } from './pages/UserList';
 
 export default function App() {
   return (
     <BrowserRouter>
       <div className="min-h-screen bg-gray-100 text-gray-800">
         <Routes>
-          {/* Al entrar a la raíz, redirige automáticamente a /login */}
-          <Route path="/" element={<Navigate to="/login" replace/>} />
-
-          {/* Ruta del login */}
-          <Route path="/login" element={<Login />} />
-
-          {/* Temporalmente colocamos un placeholder para /users para que no dé error al redirigir*/}
-          <Route path="/users" element={<div className="p-8 text-center text-xl font-bold">¡Login Exitoso! (Aquí irá la tabla)</div>} />
           
+          <Route path="/" element={<Navigate to="/login" replace/>} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/users" element={<UserList />} />
+
+          {/* Placeholders temporales para los pasos 10 y 11 */}
+          <Route path="users/new" element={<div className="p-8">Vista Formulario Crear</div>}/>
+          <Route path="/users/:id" element={<div className="p-8">Vista Detalle</div>}/>
+          <Route path="/users/:id/edit" element={<div className="p-8">Vista Formulario Editar</div>}/>
+
         </Routes>
       </div>
     </BrowserRouter>
